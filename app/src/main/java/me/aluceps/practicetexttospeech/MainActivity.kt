56c7e1f8b971bf64b16a9b1d8da0de3c120b4666
Atsushi Takano <aluceps@gmail.com>
2019-06-29 23:44:27 +0900

    Implement TextToSpeech

--- app/src/main/java/me/aluceps/practicetexttospeech/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicetexttospeech/MainActivity.kt
@@ -1,19 +1,79 @@
 package me.aluceps.practicetexttospeech
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import me.aluceps.practicetexttospeech.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private val binding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
+    private var textToSpeech: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.readText.setOnClickListener {
+        setupButton()
+        setupTextToSpeech()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        textToSpeech = TextToSpeech(this, this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        textToSpeech?.shutdown()
+    }
+
+    override fun onInit(status: Int) {
+        when (status) {
+            TextToSpeech.SUCCESS -> {
+                Log.d("TextToSpeech", "onInit: SUCCESS")
+            }
+            TextToSpeech.STOPPED -> {
+                Log.d("TextToSpeech", "onInit: STOPPED")
+            }
+            else -> {
+                Log.d("TextToSpeech", "onInit: Error=$status")
+            }
         }
+    }
+
+    private fun setupButton() {
+        binding.readText.setOnClickListener {
+            binding.text.text.toString().toSpeech()
+        }
+    }
+
+    private fun String.toSpeech() {
+        if (isEmpty()) return
+        textToSpeech?.let { tts ->
+            if (tts.isSpeaking) return@let
+            tts.speak(this, TextToSpeech.QUEUE_FLUSH, null, "")
+        }
+    }
+
+    private fun setupTextToSpeech() {
+        val result = textToSpeech?.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {
+                Log.d("TextToSpeech", "status: $utteranceId")
+            }
+
+            override fun onDone(utteranceId: String?) {
+                Log.d("TextToSpeech", "status: $utteranceId")
+            }
+
+            override fun onError(utteranceId: String?) {
+                Log.d("TextToSpeech", "status: $utteranceId")
+            }
+        })
+        Log.d("TextToSpeech", "status: $result")
     }
 }
