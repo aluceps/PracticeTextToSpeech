57018836eb220563d3ae7314e978c30755d420c6
Atsushi Takano <aluceps@gmail.com>
2019-06-29 22:44:50 +0900

    Migrate to androidx

--- app/src/main/java/me/aluceps/practicetexttospeech/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicetexttospeech/MainActivity.kt
@@ -1,12 +1,19 @@
 package me.aluceps.practicetexttospeech
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import me.aluceps.practicetexttospeech.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.readText.setOnClickListener {
+        }
     }
 }
